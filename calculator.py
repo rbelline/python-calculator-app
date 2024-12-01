accfff020f1f6e2fa450d546ae9d218fbec55b84
rbellinello <bellinelloriccardo@gmail.com>
2024-12-01 12:36:05 +0100

    new GUI finished

--- calculator.py
+++ calculator.py
@@ -3,7 +3,11 @@ import tkinter as tk
 LARGE_FONT_STYLE = ("Arial", 40, "bold")
 SMALL_FONT_STYLE = ("Arial", 16)
 DIGIT_FONT_STYLE = ("Arial", 24, "bold")
+DEFAULT_FONT_STYLE = ("Arial", 20)
+
+OFF_WHITE ="#F8FAFF"
 WHITE = "#FFFFFF"
+LIGHT_BLUE ="#CCEDFF"
 LIGHT_GRAY = "#F5F5F5"
 LABEL_COLOR = "#252653"
 class Calculator:
@@ -25,8 +29,22 @@ class Calculator:
             1:(3,1), 2:(3,2), 3:(3,3),
             0:(4,2), '.':(4,1)
         }
+        self.operators = {"/": "\u00F7", "*": "\u00D7", "-": "-", "+": "+"}
         self.buttons_frame = self.create_buttons_frame() # creation of button frame
+
+        self.buttons_frame.rowconfigure(0, weight=1)
+        # method to fill empty spaces in a grid
+        for x in range(1,5):
+            self.buttons_frame.rowconfigure(x, weight=1)
+            self.buttons_frame.columnconfigure(x, weight=1)
+
         self.create_digit_buttons()
+        self.create_operator_button()
+        self.create_special_buttons()
+
+    def create_special_buttons(self):
+        self.create_clear_button()
+        self.create_equal_button()
 
     def create_display_labels(self):
         total_label = tk.Label(self.display_frame, text=self.total_expression, anchor=tk.E, 
@@ -47,6 +65,24 @@ class Calculator:
             button = tk.Button(self.buttons_frame, text=str(digit), bg=WHITE, fg=LABEL_COLOR,
                                font=DIGIT_FONT_STYLE, borderwidth=0)
             button.grid(row=grid_value[0], column=grid_value[1], sticky=tk.NSEW)
+
+    def create_operator_button(self):
+        i = 0
+        for operator, symbol in self.operators.items():
+            button = tk.Button(self.buttons_frame, text=symbol, bg=OFF_WHITE, fg=LABEL_COLOR,
+                               font=DEFAULT_FONT_STYLE, borderwidth=0)
+            button.grid(row=i, column=4, sticky=tk.NSEW)
+            i += 1
+
+    def create_clear_button(self):
+        button = tk.Button(self.buttons_frame, text="C", bg=OFF_WHITE, fg=LABEL_COLOR,
+                               font=DEFAULT_FONT_STYLE, borderwidth=0)
+        button.grid(row=0, column=1, columnspan=3, sticky=tk.NSEW)
+
+    def create_equal_button(self):
+        button = tk.Button(self.buttons_frame, text="=", bg=LIGHT_BLUE, fg=LABEL_COLOR,
+                               font=DEFAULT_FONT_STYLE, borderwidth=0)
+        button.grid(row=4, column=3, columnspan=2, sticky=tk.NSEW)
 
     def create_buttons_frame(self):
         frame = tk.Frame(self.window)
